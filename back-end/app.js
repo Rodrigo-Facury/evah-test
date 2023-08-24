25751b9acc67f939e9dfd061c326a689beb0c884
Rodrigo <rodrigo.facury14@gmail.com>
2023-08-24 16:08:57 -0300

    feat: habilita cors

--- back-end/app.js
+++ back-end/app.js
@@ -5,21 +5,21 @@ const errorHandler = require('./api/middlewares/errorHandler');
 
 const app = express();
 
-// const whitelist = process.env.WHITELIST ? JSON.parse(process.env.WHITELIST) : ['http://localhost:5173'];
+const whitelist = process.env.WHITELIST ? JSON.parse(process.env.WHITELIST) : ['http://localhost:5173'];
 
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   }
-// }
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
 app.use(express.json());
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 app.use('/user', userRoutes);
 
